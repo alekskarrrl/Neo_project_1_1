043201a8a519ed5e549a7a96f30fd3e81a4a503d
alekskarrrl <alekskarrrl@gmail.com>
2023-08-20 19:09:50 +0300

    Project 1.3 - fixed formatting and comments

--- create_1_3_log_info.sql
+++ create_1_3_log_info.sql
@@ -2,6 +2,9 @@ CREATE SEQUENCE IF NOT EXISTS logs.seq_log_id;
 --SELECT currval('logs.seq_log_id');
 --SELECT nextval('logs.seq_log_id');
 
+-- SEQUENCE dm.seq_lg_messages --
+CREATE SEQUENCE IF NOT EXISTS dm.seq_lg_messages;
+
 CREATE TABLE IF NOT EXISTS LOGS.LOG_INFO (
 	record_id				integer,
 	pipeline_run_id			integer,
